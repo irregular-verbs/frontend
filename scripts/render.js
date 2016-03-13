23b8ccd6fac6b4de02b279fd39aaff78e82e615c
Nikolai Mavrenkov <koluch@koluch.ru>
2016-03-13 16:17:00 +0300

    Make static page generation using js generators

--- scripts/render.js
+++ scripts/render.js
@@ -5,28 +5,34 @@ import fs from 'fs'
 import WordList from './components/WordList'
 import data from './data'
 
-var template = fs.readFileSync( './static/index.html').toString(); //todo:toString - bad
-
-function renderState(rootElement, props, fileName) {
-    var element = React.createElement(rootElement, props);
-    const reactOutput = ReactDOMServer.renderToStaticMarkup(element)
-    var serializeDocument = template.replace('<div id="react"></div>', '<div id="react">'+reactOutput+'</div>')
-    fs.writeFile(fileName, serializeDocument, (err) => {
-        if (err) throw err;
-    })
+function renderState(rootElement, stateIterable, templateFileName) {
+    let template = fs.readFileSync( templateFileName ).toString(); //todo:toString - bad
+    for(let {props,fileName} of stateIterable) {
+        var element = React.createElement(rootElement, props);
+        const reactOutput = ReactDOMServer.renderToStaticMarkup(element)
+        var serializeDocument = template.replace('<div id="react"></div>', '<div id="react">'+reactOutput+'</div>')
+        console.log(`write ${fileName}`);
+        fs.writeFile(fileName, serializeDocument, (err) => {
+            if (err) throw err;
+        })
+    }
 }
 
-data.forEach(function(word)  {
-    var props = {
-        initialState: {
-            currentWordId: word.id
-        },
-        data
-    };
-    var fileName = `./debug/${word.id}.html`;
-    renderState(WordList, props, fileName)
-})
+function* generator(data) {
+    for(let word of data) {
+        var props = {
+            initialState: {
+                currentWordId: word.id
+            },
+            data
+        };
+        var fileName = `./debug/${word.id}.html`;
+        yield {
+            props,
+            fileName
+        }
+    }
+}
 
-
-
-//$ babel-node --presets es2015 render.js
+renderState(WordList, generator(data), './static/index.html')
+//$ babel-node --presets es2015 ./scripts/render.js
